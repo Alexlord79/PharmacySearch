ffbd8cf9634bba3675a5a4fb2e0cf9d11bf59834
Alexlord79 <oranskiy79@yandex.ru>
2025-10-03 11:50:37 +0300

    1.1-Database-revision переписал)) все таблицы: переделал структуру данных всех сущностей в DbModal. из таблиц осталось доделать PharmacyDbModel

--- app/src/main/java/ru/hwdoc/pharmacysearch/data/local/entity/PersonDbModel.kt
+++ app/src/main/java/ru/hwdoc/pharmacysearch/data/local/entity/PersonDbModel.kt
@@ -1,5 +1,6 @@
 package ru.hwdoc.pharmacysearch.data.local.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
@@ -7,14 +8,18 @@ import androidx.room.PrimaryKey
 @Entity(
     tableName = "persons",
     indices = [
-        Index(value = ["fullName"]),
+        Index(value = ["full_name"]),   // индекс на ФИО сотрудника
     ]
 )
 data class PersonDbModel(
     @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,            //id в локальной БД
-    val fullName: String,      //полное ФИО
-    val position: String?,       //должность
-    val mobilePhone: String?,   //мобильный телефон
-    val email: String?          //почта Email
+    val id: Int = 0,                //id в локальной БД
+    @ColumnInfo(name = "full_name", defaultValue = "нет_данных")
+    val fullName: String,           //полное ФИО
+    @ColumnInfo(name = "phone_number", defaultValue = "нет_данных")
+    val phoneNumber: String,        //мобильный телефон
+    @ColumnInfo(defaultValue = "нет_данных")
+    val position: String,           //должность
+    @ColumnInfo(defaultValue = "нет_данных")
+    val email: String               //почта Email
 )
