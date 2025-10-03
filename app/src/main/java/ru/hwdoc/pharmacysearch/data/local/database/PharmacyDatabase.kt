bb9477bb023bccb47faee3ef9f50428e22ae76c7
Alexlord79 <oranskiy79@yandex.ru>
2025-10-03 23:51:32 +0300

    1.2-Database-revision дописал БД класс аптеки, так же написал класс получения данных со всех связанных таблиц в класс аптеки (PharmacyWithAllDataDbModel), начал писать PharmacyDao

--- app/src/main/java/ru/hwdoc/pharmacysearch/data/local/database/PharmacyDatabase.kt
+++ app/src/main/java/ru/hwdoc/pharmacysearch/data/local/database/PharmacyDatabase.kt
@@ -3,7 +3,6 @@ package ru.hwdoc.pharmacysearch.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import ru.hwdoc.pharmacysearch.data.local.dao.PharmacyDao
-import ru.hwdoc.pharmacysearch.data.local.entity.AvailableDaysDbModel
 import ru.hwdoc.pharmacysearch.data.local.entity.InternetProviderDbModel
 import ru.hwdoc.pharmacysearch.data.local.entity.LegalEntityDbModel
 import ru.hwdoc.pharmacysearch.data.local.entity.PersonDbModel
@@ -16,11 +15,12 @@ import ru.hwdoc.pharmacysearch.data.local.entity.VsaDbModel
             VsaDbModel::class,
             PersonDbModel::class,
             LegalEntityDbModel::class,
-            InternetProviderDbModel::class,
-            AvailableDaysDbModel::class
+            InternetProviderDbModel::class
                ],
     version = 1,
-    exportSchema = false)
+    exportSchema = false
+)
+
 abstract class PharmacyDatabase: RoomDatabase() {
     abstract fun pharmacyDao(): PharmacyDao
 }
